dba4519e73fbe9084cc8f9879dc1671971879ed9
CrispyChillies <phamnguyenhailongct@gmail.com>
2025-08-20 09:23:34 +0700

    add game board and aggregate functions

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math/rand"
+
+	"github.com/eiannone/keyboard"
 )
 
 // import "time"
@@ -56,7 +59,7 @@ func (b *Board) MoveLeft() {
 	for r := 0; r < Size; r++ { // Changed from Size - 1 to Size
 		slice := mergeRowLeft(b.cells[r][:])
 		// Convert slice back to array
-		for i := 0; i < Size; i++ {
+		for i := range Size {
 			b.cells[r][i] = slice[i]
 		}
 	}
@@ -77,7 +80,7 @@ func (b *Board) MoveRight() {
 
 func (b *Board) Transpose() {
 	for r := range b.cells {
-		for c := range b.cells[r] {
+		for c := r + 1; c < Size; c++ {
 			b.cells[r][c], b.cells[c][r] = b.cells[c][r], b.cells[r][c]
 		}
 	}
@@ -95,18 +98,80 @@ func (b *Board) MoveDown() {
 	b.Transpose()
 }
 
+func (b *Board) IsGameOver() bool {
+	// check for empty cells
+	for r := 0; r < Size; r++ {
+		for c := 0; c < Size; c++ {
+			if b.cells[r][c] == 0 {
+				return false
+			}
+		}
+	}
+
+	// check for possible merges horizontally
+	for r := 0; r < Size; r++ {
+		for c := 0; c < Size-1; c++ {
+			if b.cells[r][c] == b.cells[r][c+1] {
+				return false
+			}
+		}
+	}
+
+	// check for possible merges vertically
+	for r := 0; r < Size-1; r++ {
+		for c := 0; c < Size; c++ {
+			if b.cells[r][c] == b.cells[r+1][c] {
+				return false
+			}
+		}
+	}
+
+	// no moves left
+	return true
+}
+
 func main() {
 	var b Board
-	b.cells[0][1] = 16
-	b.cells[0][2] = 16
 	b.AddRandomTile()
 	b.AddRandomTile()
-	b.Print()
 
-	b.MoveLeft()
-	b.Print()
+	if err := keyboard.Open(); err != nil {
+		log.Fatal(err)
+	}
 
-	// b.MoveRight()
-	// b.Print()
+	defer keyboard.Close()
+
+	for {
+		if b.IsGameOver() {
+			fmt.Println("Game Over! No moves left")
+		}
+
+		b.Print()
+		fmt.Println("Use W/A/S/D to move, Q to quit")
+
+		char, _, err := keyboard.GetSingleKey()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if char == 'q' || char == 'Q' {
+			fmt.Println("Goodbye!")
+			break
+		}
+
+		switch char {
+		case 'a', 'A':
+			b.MoveLeft()
+		case 'd', 'D':
+			b.MoveRight()
+		case 'w', 'W':
+			b.MoveUp()
+		case 's', 'S':
+			b.MoveDown()
+		}
+
+		// add new random tile after a move
+		b.AddRandomTile()
+	}
 
 }
